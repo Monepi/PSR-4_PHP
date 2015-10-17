26af38c5dab4001956667e8ac4c5dbd77f0d5b4e
Simon Mohoalali <simon@4waysunited.org.za>
2015-10-17 04:16:02 +0200

    Initial commit

---
+++ app/Codecourse/Filters/AuthFilter.php
@@ -0,0 +1,15 @@
+<?php
+
+    namespace Codecourse\Filters;
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    class AuthFilter
+    {
+
+        public function __construct()
+        {
+            die('Auth Filter');
+        }
+
+    }
---
+++ app/Codecourse/Repositories/UserRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+    namespace Codecourse\Repositories;
+    ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+
+    class UserRepository
+    {
+        public function __construct()
+        {
+            die('Hello.');
+        }
+    }
